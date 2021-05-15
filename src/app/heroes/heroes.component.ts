beca40b0983c0cd327ad7b1a3755c6bbe8459601
Pedro F. Neto <pfn.pedrofneto@gmail.com>
2021-05-15 15:33:57 -0300

    feat: :sparkles: Listando heroes mockados com ngFor e exibindo os heroes selecionados com click event binding
    Displaying mocked heroes list with ngFor and viewing the heroes details with click event binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
-  hero: Hero =  {
-    id: 1,
-    name: 'All Might',
-  }
-
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'All Migth' },
+  { id: 12, name: 'Eraser Head' },
+  { id: 13, name: 'Gran Torino' },
+  { id: 14, name: 'Deku' },
+  { id: 15, name: 'Dynamight' },
+  { id: 16, name: 'Tsukuyomi' },
+  { id: 17, name: 'Red Riot' },
+  { id: 18, name: 'Suneater' },
+  { id: 19, name: 'Lemillion' },
+  { id: 20, name: 'Mirko' },
+  { id: 21, name: 'Best Jeanist' },
+  { id: 22, name: 'Hawks' },
+  { id: 23, name: 'Endeavor' },
+  { id: 24, name: 'Gang Orca' },
+  { id: 25, name: 'Fat Gum' },
+];
